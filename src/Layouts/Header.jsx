b7b8478290d3883773b201370e7deef32419f57d
golah-777 <128902542+golah-777@users.noreply.github.com>
2024-03-16 22:14:16 +0200

    Header

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,17 +1,58 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import './Header.css'
+import "./Header.css";
+import mainLogo from "./amazon-logo-free-png.webp";
+
 export default function Header() {
   return (
     <header className="header">
       <nav>
         <ul>
-          <li>
-            <NavLink activeClassName="active" to="/"> Home</NavLink>
+          <div className="logo">
+            <NavLink to="/">
+              <img src={mainLogo} alt="" className="logo-img" />
+            </NavLink>
+          </div>
+          <div className="nav-links">
+            <i class="material-icons">place</i>
+            <div>
+              <span>Deliver to</span>
+              <h4>South Africa</h4>
+            </div>
+          </div>
+          <div className="search">
+            <input type="search" placeholder="Seach Amazon" />
+            <div>
+              <i class="material-icons">search</i>
+            </div>
+          </div>
+          <NavLink>
+            <div div className="nav-links-text">
+              <span>Hello,</span>
+              <h4>Sign In</h4>
+            </div>
+          </NavLink>
+          <div className="nav-links-text">
+            <span>Returns</span>
+            <h4>& Orders</h4>
+          </div>
+          <NavLink>
+            <div className="nav-links">
+              <h4 className="items-no">0</h4>
+              <span class="material-icons cart">shopping_cart</span>
+              <h4>Cart</h4>
+            </div>
+          </NavLink>
+          {/* <li>
+            <NavLink activeClassName="active" to="/">
+              {" "}
+              Home
+            </NavLink>
           </li>
-          <li>
-            <NavLink activeClassName="active" to="/products">Products</NavLink>
-          </li>
+          <div>
+            <NavLink activeClassName="active" to="/products">
+            </NavLink>
+          </div> */}
         </ul>
       </nav>
     </header>
